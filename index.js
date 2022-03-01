02154bbee084d27becb86da028cac191bb4eb1e3
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2022-02-28 22:30:37 -0700

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+import { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({port:6969});
+
+let shouldSendStart = false;
+let lastState = {};
+let clients = [];
+
+wss.on('connection', ws =>
+{
+    clients.push(ws);
+    console.log("Connected client.");
+
+    ws.on('close', () =>
+    {
+        console.log("Client disconnected.");
+    });
+
+    ws.on('message', msg =>
+    {
+        let packetBuffer = JSON.parse(msg);
+        if (packetBuffer.type == "admin")
+        {
+            for (let client of clients)
+            {
+                client.send(JSON.stringify(packetBuffer));
+            }
+        }
+
+        if (msg.data == 'admin_start')
+        {
+            shouldSendStart = true;
+        }
+
+        if (packetBuffer.type == 'ping')
+        {
+            // ws.send(JSON.stringify(lastPacketBuffer));
+        }
+    });
+
+    setTimeout(() => {
+        ws.send('start');
+    }, 1000);
+
+});
